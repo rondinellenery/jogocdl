4f7e03d7b49c77e3fd4395a57a2217fcc2343f68
rondy <rondipaper@gmail.com>
2026-02-23 19:55:39 -0300

    Cliado linha quadrado e oval

--- src/main/java/com/cdl/jogo/Janela.java
+++ src/main/java/com/cdl/jogo/Janela.java
@@ -3,6 +3,7 @@ package com.cdl.jogo;
 import java.awt.Graphics;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import java.awt.Color;
 
 public class Janela extends JFrame {
     private JPanel tela;
@@ -11,7 +12,10 @@ public class Janela extends JFrame {
 
         tela = new JPanel(){
             public void paintComponent(Graphics g){
-                
+                g.setColor(Color.BLUE);
+                g.drawLine(0, 240, 640, 240);
+                g.drawRect(10, 25, 20, 20);
+                g.drawOval(30, 20, 40, 30);
             }
 
         };
